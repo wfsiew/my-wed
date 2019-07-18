9999fd2fd30be779feddc47ec70a5fac0c4741ec
Siew Wing Fei <siewwingfei@hotmail.com>
2019-07-18 11:46:28 +0800

    j

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AppConstant } from '../constants/index';
 
 @Component({
@@ -11,7 +10,7 @@ export class HomeComponent implements OnInit {
 
   readonly AppConstant = AppConstant;
 
-  constructor(private router: Router) { 
+  constructor() { 
   }
 
   ngOnInit() {
